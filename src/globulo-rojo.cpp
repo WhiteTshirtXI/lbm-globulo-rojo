7c0dab95390c48674fa7f0cdaadcf64d4e35f768
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2013-09-13 16:18:14 -0500

    Fixed some compilation errors

--- src/globulo-rojo.cpp
+++ src/globulo-rojo.cpp
@@ -6,18 +6,22 @@
 #include "ibm.h"
 #include "fluid.h"
 #include "mesh.h"
+#include "memory.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	mesh membrana, referencia;
-	fluid fluido;
-	double dt = 1.0;
-	double dx = 1.0;
 	double X = 21;
 	double Y = 21;
 	double Z = 21;
+
+	mesh membrana, referencia;
+	fluid fluido(X, Y, Z);
+
+	double dt = 1.0;
+	double dx = 1.0;
+
 	int VTK = 50;
 
 	// Parametros adimensionales
@@ -52,12 +56,30 @@ int main(int argc, char *argv[])
 	referencia.moverCentro((X-1)/2.0, (Y-1)/2.0, (Z-1)/2.0);
 	referencia.iniciarGeometria();
 
+	float *cells_f = fluido.get_cells();
+	float *flags_f = fluido.get_flags();
+	float *rho_f = fluido.get_rho();
+	float *fuerza_f = fluido.get_fuerza();
+	float *vel_f = fluido.get_vel();
+
+	float *cells_d = NULL;
+	float *flags_d = NULL;
+	float *rho_d = NULL;
+	float *fuerza_d = NULL;
+	float *vel_d = NULL;
+
+	alloc_memory_GPU(X, Y, Z, &cells_d, &flags_d, &vel_d, &rho_d, &fuerza_d);
+
+	send_data_to_GPU(X, Y, Z, cells_f, cells_d, flags_f, flags_d, vel_f, vel_d, rho_f, rho_d, fuerza_f, fuerza_d);
+
+
 	// Fluido
-	fluido.inicializar(X,Y,Z);
+	//From fluid constructor
+	fluido.calcularMacro(cells_d, rho_d, vel_d, fuerza_d);
 	fluido.setVelocidad(gamma_dot);
 
 	for(int ts = 0 ; ts < STEPS ; ts++)
-		{
+	{
 		// -----------------------------------------------------------------------//
 		// 1. Interpolation
 		// -----------------------------------------------------------------------//
@@ -82,13 +104,13 @@ int main(int argc, char *argv[])
 		// -----------------------------------------------------------------------//
 		// 5. Solucionar la dinámica del fluido
 		// -----------------------------------------------------------------------//
-		fluido.collide();
-		fluido.stream();
+		fluido.collide(cells_d, fuerza_d);
+		fluido.stream(cells_d, flags_d);
 
 		// -----------------------------------------------------------------------//
 		// 6. Calcular propiedades macro del fluido
 		// -----------------------------------------------------------------------//
-		fluido.calcularMacro();
+		fluido.calcularMacro(cells_d, rho_d, vel_d, fuerza_d);
 
 		// -----------------------------------------------------------------------//
 		// 7. Calcular propiedades macro de la membrana
@@ -100,11 +122,16 @@ int main(int argc, char *argv[])
 		// -----------------------------------------------------------------------//
 		if(ts%VTK==0)
 		{
+			retrieve_data_from_GPU(X, Y, Z, cells_f, cells_d, flags_f, flags_d, vel_f, vel_d, rho_f, rho_d, fuerza_f, fuerza_d);
+
 			fluido.guardar(ts);
 			membrana.guardarVTU(ts);
 			printf("%d\n",ts);
 		}
 	}//Ciclo principal
+
+	free_memory_GPU(cells_d, flags_d, vel_d, rho_d, fuerza_d);
+
 
 	return 0;
 }
