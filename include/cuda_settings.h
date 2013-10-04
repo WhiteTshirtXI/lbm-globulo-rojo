9993a7739e35cbec9edeede88966d1a15de4f140
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2013-10-04 10:44:02 -0500

    Included cuda_settings (TUNING)

---
+++ include/cuda_settings.h
@@ -0,0 +1,14 @@
+#ifndef _CUDA_SETTINGS_H_
+#define _CUDA_SETTINGS_H_
+
+#define GRID_SIZE_X 13
+#define GRID_SIZE_Y 1
+#define GRID_SIZE_Z 3
+
+//Maximum number of threads per block:           1024
+
+#define BLOCK_SIZE_X 4
+#define BLOCK_SIZE_Y 32
+#define BLOCK_SIZE_Z 8
+
+#endif
