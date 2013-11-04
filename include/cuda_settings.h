907645b468be2a3c88e9ccc65928f23bb2ff80ff
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2013-11-04 13:42:42 -0500

    Added membrana 2

--- include/cuda_settings.h
+++ include/cuda_settings.h
@@ -1,14 +1,14 @@
 #ifndef _CUDA_SETTINGS_H_
 #define _CUDA_SETTINGS_H_
 
-#define GRID_SIZE_X 13
-#define GRID_SIZE_Y 1
-#define GRID_SIZE_Z 3
+#define GRID_SIZE_X 6
+#define GRID_SIZE_Y 6
+#define GRID_SIZE_Z 6
 
 //Maximum number of threads per block:           1024
 
-#define BLOCK_SIZE_X 4
-#define BLOCK_SIZE_Y 32
-#define BLOCK_SIZE_Z 8
+#define BLOCK_SIZE_X 10
+#define BLOCK_SIZE_Y 10
+#define BLOCK_SIZE_Z 10
 
 #endif
